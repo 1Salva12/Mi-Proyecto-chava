00aba6410dacca0262c25867b3bc316b1a201da0
1Salva12 <154624961+1Salva12@users.noreply.github.com>
2025-04-21 21:51:22 -0600

    Add files via upload

---
+++ Constantes.java
@@ -0,0 +1,22 @@
+public class Constantes {
+    public static void main(String[] args){
+
+    System.out.println("*** Constante en Java ***");
+    final var Dias_Semanas = 7; 
+
+    System.out.println("Dias_Semanas" + Dias_Semanas); 
+
+    //Dias_Semanas=8
+
+    final var PI = 3.1416 ;
+    System.out.println("PI =" + PI);
+    System.out.println("Math.PI = " + Math.PI);
+
+    final var Mensaje_Bienvenida = "Bienvenido a la Universidad java";
+    System.out.println("Mensaje_Bienvenido = " + Mensaje_Bienvenida);
+
+    final var Minuto_Por_Segundo = 60;
+    System.out.println("Minuto_Por_Segundo =" + Minuto_Por_Segundo);
+    
+ }
+}
